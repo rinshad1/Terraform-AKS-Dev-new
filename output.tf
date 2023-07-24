87819145ca2092d102522cca2ca6ee15b5cbf2cf
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-07-24 15:40:57 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,3 +9,20 @@ output "kube_config" {
 output "fqdn" {
   value = azurerm_kubernetes_cluster.k8s.fqdn
 }
+output "resourcename" {
+  value       = var.resourcename
+  description = "The Resource Name of AKS cluster"
+}
+output "cluster_name" {
+  value       = var.clustername
+  description = "The name of the AKS cluster"
+}
+output "vnet_name" {
+  value       = var.vnetname
+  description = "The vnet name of the AKS cluster"
+}
+
+output "command_line_access" {
+  value       = "az aks get-credentials --resource-group var.resourcename --name var.clustername"
+  description = "Configure kubectl command line access by running the following command"
+}
