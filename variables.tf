287183e7d4253d7590755b79809a881fd25e3862
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-08-07 15:22:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "dnspreffix" {
   default = "kubecluster-test"
 }
 variable "size" {
-  default = "Standard_D2_v2"
+  default = "Standard DS2_v2"
 }
 variable "agentnode" {
   default = "1"
