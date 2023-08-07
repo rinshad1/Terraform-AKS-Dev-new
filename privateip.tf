9db1c6a14fe2292e50235a2c005b9e3de47f06ba
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-08-07 14:19:01 +0530

    Update privateip.tf

--- privateip.tf
+++ privateip.tf
@@ -1,9 +1,9 @@
 resource "azurerm_network_interface" "example" {
-  name                = "example-nic-dev"
+  name                = "example-nic-test"
   location            = azurerm_resource_group.k8s.location
   resource_group_name = azurerm_resource_group.k8s.name
   ip_configuration {
-    name                          = "example-nic-ipconfig-dev"
+    name                          = "example-nic-ipconfig-test"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = null
