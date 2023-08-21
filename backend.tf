d5cb6261dcb64aeaa5775cb1dae0d9fd11d710ba
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-08-21 12:09:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "DefaultResourceGroup-EUS"
-    storage_account_name = "tstateterraform01"
+    storage_account_name = "tstateterraform02"
     container_name       = "tfstate-dev"
     key                  = "terraform.tfstate"
   }
